4dc671c1cac1fc600d0310995d78d0d8a5374dac
Westalgie <66008401+Westalgie@users.noreply.github.com>
2024-09-07 13:39:30 +0300

    Fix typo in mod_limitaed_base usage (#3)

--- A3UE/addons/ultimate/config/cfgForbiddenItemsMod.hpp
+++ A3UE/addons/ultimate/config/cfgForbiddenItemsMod.hpp
@@ -12,4 +12,4 @@ class mod_limited_base : forbidden_limited_base
 
 class Equipment_Classname : mod_unlimited_base {}; // Will not appear in crates, but can be unlimited in the arsenal
 
-class Equipment_Classname_2 : mod_unlimited_base {}; // Will not appear in crates, and can't be unlimited in the arsenal
+class Equipment_Classname_2 : mod_limited_base {}; // Will not appear in crates, and can't be unlimited in the arsenal
